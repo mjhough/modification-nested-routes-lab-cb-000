bd8f19c7db83a2a43aabe3900d764a67df013561
Matt Hough <matt@hough.tv>
2017-12-18 07:17:43 +1000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,6 +26,12 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    if params[:artist_id] && Artist.find_by(id: params[:artist_id])
+      @nested = true
+      @song.artist = @artist if @artist = Artist.find_by(id: params[:artist_id])
+    elsif params[:artist_id] && !Artist.find_by(id: params[:artist_id])
+      redirect_to artists_path
+    end
   end
 
   def create
@@ -39,7 +45,14 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    @song = Song.find_by(id: params[:id])
+    @artist = Artist.find_by(id: params[:artist_id])
+    @nested = true if params[:artist_id]
+    if !@song
+      redirect_to artist_songs_path(@artist)
+    elsif params[:artist_id] && @song.artist != @artist
+      redirect_to artists_path
+    end
   end
 
   def update
@@ -64,7 +77,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
